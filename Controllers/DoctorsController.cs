70ec777b722c9f02d6ff55f0750317ef7056bebd
abdelrahmanamr54 <111569311+abdelrahmanamr54@users.noreply.github.com>
2024-03-14 00:57:35 +0200

    Add files via upload

---
+++ Controllers/DoctorsController.cs
@@ -0,0 +1,24 @@
+﻿using Hospital.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        public IActionResult BookApointment()
+        {
+            SampleDataDoctor sample = new SampleDataDoctor();
+            List<Doctor> res = sample.doctors.ToList();
+
+            return View(res);
+        }
+        public IActionResult completeApointment(int id)
+        {
+            SampleDataDoctor sample = new SampleDataDoctor();
+
+            var res = sample.doctors.Find(e => e.Id == id);
+            return View(res);
+
+        }
+    }
+}
